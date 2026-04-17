376402df477c7c9579b3691ee172741d557a9458
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-04-17 16:29:04 +0900

    Refactor _nmos module to use assign statement

--- _nmos.sv
+++ _nmos.sv
@@ -1,16 +1,10 @@
 module _nmos
 (
   output wire outputData,
-  input wire inputData,
-  input wire Control
+  input  wire inputData,
+  input  wire Control
 );
-  always @(*)
-  begin
-    if(Control == 1'b1)
-      outputData=inputData;
-    else if(Control ==1'b0)
-      outputData=1'bz;
-    else
-      outputData= 1'bx;
-  end
+  assign outputData = (Control == 1'b1) ? inputData :
+                      (Control == 1'b0) ? 1'bz :
+                                          1'bx;
 endmodule
