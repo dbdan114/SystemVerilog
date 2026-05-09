068953d1325aba39554f2b451a84d60fedf7e1de
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-05-09 15:22:53 +0900

    Add _not module for tri-state logic

---
+++ _not.sv
@@ -0,0 +1,10 @@
+module _not
+(
+  output tri outputData,
+  input tri [1:0] DigitSupply,
+  input triinputData
+);
+  _pmos For0(outputData,DigitSupply[1],inputData);
+  _nmos For1(outputData,DigitSupply[0],inputData);
+    
+endmodule
