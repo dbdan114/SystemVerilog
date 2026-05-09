7c62b0722c75346ff21395e2c1e4eb569f2c5a5d
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-05-09 15:25:56 +0900

    Fix input declaration in _not.sv module

--- _not.sv
+++ _not.sv
@@ -2,7 +2,7 @@ module _not
 (
   output tri outputData,
   input tri [1:0] DigitSupply,
-  input triinputData
+  input tri inputData
 );
   _pmos For0(outputData,DigitSupply[1],inputData);
   _nmos For1(outputData,DigitSupply[0],inputData);
