8909b2f0e6a2f410e1d5ca639229998bd4297388
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-22 07:25:48 +0900

    Fix formatting of for loop in _nor.sv

---
+++ _nor.sv
@@ -0,0 +1,46 @@
+module _nor
+#(
+  parameter INPUT_WIDTH = 1
+)
+(
+  output tri outputData,
+  input wire Logic0,
+  input wire Logic1,
+  input [INPUT_WIDTH-1:0] inputData
+);
+  tri  [INPUT_WIDTH:0] Temp;
+  nmos InitializeTo1
+  (
+    Temp[0],
+    Logic1,
+    Logic1
+  );  
+  genvar Index;
+  generate
+    for
+    (
+      Index=0;
+      Index < INPUT_WIDTH
+      Index = Index + 1
+    )
+    begin:Place_and_nB_to_A
+      and_nB_to_A Paste
+      (
+        Temp[Index+1],
+        Logic0,
+        Logic1,
+        Temp[Index],
+        inputData[Index]
+      );
+    end
+  endgenerate
+  
+  _buf ReturnValue
+  (
+    outputData,
+    Logic0,
+    Logic1,
+    Temp[INPUT_WIDTH]
+  );
+    
+endmodule
