8d680fe6db182139f76aa507a2841556d18c39d0
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-21 12:28:42 +0900

    Implement _or module with parameterized input width

---
+++ _or.sv
@@ -0,0 +1,43 @@
+module _or
+#(
+  parameter INPUT_WIDTH = 1
+) 
+(
+  output tri outputData,
+  input wire Logic0,
+  input wire Logic1,
+  input tri [INPUT_WIDTH-1:0] inputData
+);
+  tri  [INPUT_WIDTH:0] Temp;
+  
+  nmos InitializeTo0
+  (
+    Temp[0],
+    Logic0,
+    Logic1
+  );
+  
+  genvar Index;
+  
+  generate
+    for(Index=0; Index < INPUT_WIDTH; Index = Index + 1)
+    begin:Place_or_B_to_A
+      or_B_to_A Paste
+      (
+        Temp[Index+1],
+        Logic0,
+        Logic1,
+        Temp[Index],
+        inputData[Index]
+      );
+    end
+  endgenerate
+
+  nmos InitializeTo0
+  (
+    outputData,
+    Temp[INPUT_WIDTH],
+    Logic1
+  );
+    
+endmodule
