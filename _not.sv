edd6efed7279178dd52282e85cb4622535483c67
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-21 10:01:55 +0900

    Update _not.sv
    생활비가 모자라 힘들어요. {~.~}

--- _not.sv
+++ _not.sv
@@ -1,10 +1,20 @@
 module _not
 (
   output tri outputData,
-  input tri [1:0] DigitSupply,
+  input wire Logic0,
+  input wire Logic1,
   input tri inputData
 );
-  _pmos For0(outputData,DigitSupply[1],inputData);
-  _nmos For1(outputData,DigitSupply[0],inputData);
-    
+  _pmos For0
+  (
+    outputData,
+    Logic1,
+    inputData
+  );
+  _nmos For1
+  (
+    outputData,
+    Logic0,
+    inputData
+  );
 endmodule
