d18f3d8ed88b34dd617feed953926d45d60a7878
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-21 10:20:13 +0900

    Fix syntax for pmos and nmos instantiation

--- _buf.sv
+++ _buf.sv
@@ -5,13 +5,13 @@ module _buf
   input wire Logic1,
   input tri inputData
 );
-  _pmos For0
+  pmos For0
   (
     outputData,
     Logic0,
     inputData
   );
-  _nmos For1
+  nmos For1
   (
     outputData,
     Logic1,
