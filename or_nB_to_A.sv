522de0de58f815cdb6cc865065b2c47d97dd01f0
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-21 10:15:02 +0900

    Refactor or_nB_to_A module to include Logic0 and Logic1
    살려주세요. {~.~}
    ' NH농협은행 360-12-178547 김건무'
    - {^.^}

--- or_nB_to_A.sv
+++ or_nB_to_A.sv
@@ -1,10 +1,21 @@
 module or_nB_to_A
 (
   output tri outputData,
-  input tri [1:0] DigitSupply,
+  input wire Logic0,
+  input wire Logic1,
   input tri A,
   input tri B
 );
-  _pmos For0(outputData,DigitSupply[1],B);
-  _nmos For1(outputData,A,B);
+  _pmos For0
+  (
+    outputData,
+    Logic1,
+    B
+  );
+  _nmos For1
+  (
+    outputData,
+    A,
+    B
+  );
 endmodule
