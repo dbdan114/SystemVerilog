cc8b8a378778b8d7220227b6e9f1c570997af4c4
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-21 10:08:37 +0900

    Refactor and_B_to_A module to include Logic0 and Logic1
    생활비 모자라요. 'NH농협은행 360-12-178547 김건무'에 500원만 송금해 주세요. 
    - {^.^}

--- and_B_to_A.sv
+++ and_B_to_A.sv
@@ -1,10 +1,21 @@
 module and_B_to_A
 (
   output tri outputData,
-  input tri [1:0] DigitSupply,
+  input wire Logic0,
+  input wire Logic1,
   input tri A,
   input tri B
 );
-  _pmos For0(outputData,DigitSupply[0], B);
-  _nmos For1(outputData,A, B);
+  _pmos For0
+  (
+    outputData,
+    Logic0,
+    B
+  );
+  _nmos For1
+  (
+    outputData,
+    A,
+    B
+  );
   endmodule
