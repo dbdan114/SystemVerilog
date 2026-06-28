f828daa1ec66e85aa390407e1e5aab901d54aa8b
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-06-28 17:37:30 +0900

    Refactor OpAmpSEO module input ports to inout
    Changed input ports to inout and modified types.

---
+++ OpAmpSEO.sv
@@ -0,0 +1,16 @@
+`include "DigitSupply.vh"
+
+module OpAmpSEO
+(
+  inout wire V_Plus,
+  inout tri input_Plus,
+  output output_Neutral,
+  inout tri input_Minus,
+  inout wire V_Minus
+);
+  DifferentialQBit FetchV_Plus(V_Plus,input_Plus);
+  DifferentialQBit FetchV_Minus(input_Minus,V_Minus);
+  DifferentialQBit Fetchinput_Plus(output_Neutral,output_Plus);
+  DifferentialQBit Fetchinput_Minus(output_Neutral,input_Minus);
+  
+endmodule
