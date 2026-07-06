1cbb9a4ef55b1aa44e2bd734e1c98fe80fb64f72
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 12:37:49 +0900

    Refactor PostLNA module to use tri signals

---
+++ PostLNA.sv
@@ -0,0 +1,51 @@
+`include "DigitSupply.vh"
+
+module PostLNA
+(
+  input tri OuterReceive,
+  output tri InnerReceive,
+  input tri InnerTransmit,
+  output tri OuterTransmit
+);
+
+  tri TempVss;
+  tri TempVdd;
+  
+  DifferentialQBit CorrectTempVss
+  (
+    Vss,
+    TempVss
+  );
+  DifferentialQBit FetchVss
+  
+  (
+    TempVss,
+    OuterReceive
+  );
+  DifferentialQBit FetchOuterReceive
+  (
+    OuterReceive,
+    InnerReceive
+  );
+  DifferentialQBit FetchInnerReceive
+  (
+    InnerReceive,
+    InnerTransmit
+  );
+  DifferentialQBit FetchInnerTransmit
+  (
+    InnerTransmit,
+    OuterTransmit
+  );
+  DifferentialQBit FetchOuterTransmit
+  (
+    OuterTransmit,
+    TempVdd
+  );
+  DifferentialQBit CorrectTempVdd
+  (
+    TempVdd,
+    Vdd
+  );
+  
+endmodule
