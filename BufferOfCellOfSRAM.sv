c0a84b1d5b149f80b87a89edfc4a31591d0d2b4d
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 13:29:36 +0900

    Add Logic0 and Logic1 inputs to BufferOfCellOfSRAM
    Added input wires Logic0 and Logic1 to the BufferOfCellOfSRAM module and updated connections for NorthToSouth and SouthToNorth buffers.

--- BufferOfCellOfSRAM.sv
+++ BufferOfCellOfSRAM.sv
@@ -1,7 +1,22 @@
 module BufferOfCellOfSRAM
 (
   inout tri North,
-  inout tri South
+  inout tri South,
+  input wire Logic0,
+  input wire Logic1
 );
-
+  _buf NorthToSouth
+  (
+    DataSouth,
+    Logic0,
+    Logic1,
+    DataNorth
+  );
+  _buf SouthToNorth
+  (
+    DataNorth,
+    Logic0,
+    Logic1,
+    DataSouth
+  );
 endmodule
