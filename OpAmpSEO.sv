ef1d89da084a5fac0338257c0404ea4f21513b62
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 10:41:11 +0900

    Refactor instantiation of DifferentialQBit modules

--- OpAmpSEO.sv
+++ OpAmpSEO.sv
@@ -8,9 +8,25 @@ module OpAmpSEO
   inout tri input_Minus,
   inout wire V_Minus
 );
-  DifferentialQBit FetchV_Plus(V_Plus,input_Plus);
-  DifferentialQBit FetchV_Minus(input_Minus,V_Minus);
-  DifferentialQBit Fetchinput_Plus(output_Neutral,input_Plus);
-  DifferentialQBit Fetchinput_Minus(output_Neutral,input_Minus);
+  DifferentialQBit FetchV_Plus
+  (
+    V_Plus,
+    input_Plus
+  );
+  DifferentialQBit FetchV_Minus
+  (
+    input_Minus,
+    V_Minus
+  );
+  DifferentialQBit Fetchinput_Plus
+  (
+    output_Neutral,
+    input_Plus
+  );
+  DifferentialQBit Fetchinput_Minus
+  (
+    output_Neutral,
+    input_Minus
+  );
   
 endmodule
