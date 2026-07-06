f08ca4ecc3131f0e344c0ddf4b570b2f6337236d
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 10:42:28 +0900

    Refactor DifferentialQBit module instantiations
    Refactor instantiation of DifferentialQBit modules for clarity.

--- OpAmpDO.sv
+++ OpAmpDO.sv
@@ -9,10 +9,30 @@ module OpAmpDO
   inout tri input_Minus,
   inout wire V_Minus
 );
-  DifferentialQBit FetchV_Plus(V_Plus,input_Plus);
-  DifferentialQBit FetchV_Minus(input_Minus,V_Minus);
-  DifferentialQBit Fetchinput_Plus(input_Plus,output_Plus);
-  DifferentialQBit Fetchinput_Minus(output_Minus,input_Minus);
-  DifferentialQBit output_Plus_To_output_Minus(output_Plus,output_Minus);
+  DifferentialQBit FetchV_Plus
+  (
+    V_Plus,
+    input_Plus
+  );
+  DifferentialQBit FetchV_Minus
+  (
+    input_Minus,
+    V_Minus
+  );
+  DifferentialQBit Fetchinput_Plus
+  (
+    input_Plus,
+    output_Plus
+  );
+  DifferentialQBit Fetchinput_Minus
+  (
+    output_Minus,
+    input_Minus
+  );
+  DifferentialQBit output_Plus_To_output_Minus
+  (
+    output_Plus,
+    output_Minus
+  );
   
 endmodule
