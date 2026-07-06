3208ae10c9b54fb082c6b0cc3f9f9a11d9f799c6
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 11:31:03 +0900

    Refactor SplitRadio module to improve readability

---
+++ SplitRadio.sv
@@ -0,0 +1,74 @@
+`include "DigitSupply.vh"
+
+module SplitRadio
+(
+  input tri V_Plus,
+  inout tri Receive,
+  inout tri ReceivedRadio,
+  inout tri ReceivedWire,
+  input tri V_Minus
+);
+  tri Temp_Plus1;
+  tri Temp_Plus2;
+  tri Temp_Plus3;
+  tri Temp_Minus3;
+  tri Temp_Minus2;
+  tri Temp_Minus1;
+  tri NoConnect=Vss;
+ 
+  DifferentialQBit FetchV_Plus
+  (
+    V_Plus,
+    Receive
+  );
+  DifferentialQBit FetchV_Minus
+  (
+    NoConnect,
+    V_Minus
+  );
+  DifferentialQBit FetchReceive
+  (
+    Receive,
+    Temp_Plus1
+  );
+  DifferentialQBit FetchNoConnect
+  (
+    Temp_Minus1,
+    NoConnect
+  );
+  DifferentialQBit FetchTemp_Plus1
+  (
+    Temp_Plus1,
+    Temp_Plus2
+  );
+  DifferentialQBit FetchTemp_Minus1
+  (
+    Temp_Minus2,
+    Temp_Minus1
+  );
+  DifferentialQBit FetchTemp_Plus2
+  (
+    Temp_Plus2,
+    Temp_Plus3
+  );
+  DifferentialQBit FetchTemp_Minus2
+  (
+    Temp_Minus3,
+    Temp_Minus2
+  );
+  DifferentialQBit FetchTemp_Plus3
+  (
+    Temp_Plus3,
+    ReceivedRadio
+  );
+  DifferentialQBit FetchTemp_Minus3
+  (
+    ReceivedWire,
+    Temp_Minus3
+  );
+  DifferentialQBit LinkRadioBetweenWire
+  (
+    ReceivedRadio,
+    ReceivedWire
+  );
+endmodule
