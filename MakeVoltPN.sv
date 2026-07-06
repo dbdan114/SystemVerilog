4f42df568380f01cf689e11c6c8e4f3ed64947c8
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-07-06 10:43:32 +0900

    Refactor instantiation of DifferentialQBit modules

--- MakeVoltPN.sv
+++ MakeVoltPN.sv
@@ -5,8 +5,19 @@ module MakeVoltPN
   output V_Plus,
   output V_Minus
 );
-
-  DifferentialQBit LinkVddVss(rVdd,rVss);
-  DifferentialQBit MakeV_Minus(rVss,V_Minus);
-  DifferentialQBit MakeV_Plus(V_Plus,rVdd);
+  DifferentialQBit LinkVddVss
+  (
+    rVdd,
+    rVss
+  );
+  DifferentialQBit MakeV_Minus
+  (
+    rVss,
+    V_Minus
+  );
+  DifferentialQBit MakeV_Plus
+  (
+    V_Plus,
+    rVdd
+  );
 endmodule
