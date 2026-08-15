aeefcf6eb84de21437fc829b8510828ffa495914
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-08-15 11:06:40 +0900

    Update ClockEdge assignment logic
    Modify ClockEdge assignment to depend on Vdd condition.

--- PosEdge.sv
+++ PosEdge.sv
@@ -5,8 +5,11 @@ module PosEdge
   input  tri Clock,
   output tri ClockEdge
 );
-  always_ff @(posedge Clock) begin
-    assign ClockEdge=Vdd;
-    
+  always_ff @(posedge Clock)
+  begin
+    if(Vdd)
+    begin
+      ClockEdge=Vdd;
+    end
   end
 endmodule
