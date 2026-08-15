08af675659eaf1a2e83716712d6088b11803da06
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-08-15 11:03:34 +0900

    Refactor PosEdge module for tri signals
    Updated module to use tri inputs and outputs, and added ClockEdge signal assignment.

--- PosEdge.sv
+++ PosEdge.sv
@@ -2,20 +2,11 @@
 
 module PosEdge
 (
-  input  logic clk,
-  input  logic rst_n,
-  input  logic signal,
-  output logic edge
+  input  tri Clock,
+  output tri ClockEdge
 );
-  logic signal_d;
-
-  always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      signal_d <= 1'b0;
-      edge     <= 1'b0;
-    end else begin
-      signal_d <= signal;
-      edge     <= signal & ~signal_d;
-    end
+  always_ff @(posedge Clock) begin
+    ClockEdge=Vdd;
+    
   end
 endmodule
