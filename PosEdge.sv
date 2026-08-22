41ac4e9047230ccfce144043ab0344c61e29940c
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-08-22 10:46:10 +0900

    Update PosEdge module with tri signals
    Refactor PosEdge module to use tri signals and remove unused latch.

--- PosEdge.sv
+++ PosEdge.sv
@@ -1,42 +1,12 @@
 module PosEdge
 (
-  output logic ClockEdge,
+  output tri ClockEdge,
   input  tri Clock,
   input wire Logic0,
   input wire Logic1
 );
-  tri LatchedClock;
+  tri Temp1;
+  tri Temp2;
 
-  CellOfSRAM MakeLatch
-  (
-    LatchedClock,
-    Logic1,
-    Clock,
-    Logic1,
-    Logic0,
-    Logic1
-  );
-
-  /*initial 
-  begin 
-    ClockEdge= Logic0;
-  end*/
-  
-  always_ff @(posedge Clock)
-  begin
-    if(Logic1)
-    begin
-      ClockEdge=Logic1;
-    end
-  end
-
-  always_ff @(posedge LatchedClock)
-  begin
-    if(Logic1)
-    begin
-      ClockEdge=Logic0;
-    end
-  end
-  
   
 endmodule
