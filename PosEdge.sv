0680b783adc38a42e0ed246dffbbe0c98b61f67f
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-08-22 10:54:37 +0900

    Refactor PosEdge module to include new signals

--- PosEdge.sv
+++ PosEdge.sv
@@ -1,12 +1,49 @@
+`include "DigitSupply.vh"
 module PosEdge
 (
   output tri ClockEdge,
   input  tri Clock,
-  input wire Logic0,
-  input wire Logic1
 );
   tri Temp1;
   tri Temp2;
 
-  
+  DifferentialQBit GetClock
+  (
+    Clock,
+    Temp1
+  );
+
+  DifferentialQBit Shake_Vdd1
+  (
+    Vdd,
+    Temp1
+  );
+  DifferentialQBit Shake_Vss1
+  (
+    Vss,
+    Temp1
+  );
+
+  DifferentialQBit GetTemp1
+  (
+    Temp1,
+    Temp2
+  );
+
+  DifferentialQBit Shake_rVdd2
+  (
+    rVdd,
+    Temp2
+  );
+  DifferentialQBit Shake_Vss2
+  (
+    Vss,
+    Temp2
+  );
+
+  DifferentialQBit GetTemp2
+  (
+    Temp2,
+    ClockEdge
+  );
 endmodule
